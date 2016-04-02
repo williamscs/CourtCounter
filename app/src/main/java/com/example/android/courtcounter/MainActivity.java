d916baff1eb41ec9ed238d80e42d77e1f6d24904
Chris Williams <cwilliams88@gmail.com>
2016-04-02 13:29:54 -0500

    Hook up button methods

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.courtcounter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void threePointerScored(View v){
+        displayForTeamA(3);
+    }
+
+    public void twoPointerScored(View v){
+        displayForTeamA(2);
+    }
+
+    public void freeThrowScored(View v){
+        displayForTeamA(1);
     }
 }
