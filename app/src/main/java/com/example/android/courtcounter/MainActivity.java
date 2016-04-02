3406d99a22efe1803797cfef4e838da45e54999c
Chris Williams <cwilliams88@gmail.com>
2016-04-02 13:44:33 -0500

    Create Team B layout and methods

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int scoreTeamA = 0;
+    private int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,15 +23,41 @@ public class MainActivity extends AppCompatActivity {
         scoreView.setText(String.valueOf(score));
     }
 
-    public void threePointerScored(View v){
-        displayForTeamA(3);
+    public void threePointerScoredForTeamA(View v){
+        this.scoreTeamA += 3;
+        displayForTeamA(this.scoreTeamA);
     }
 
-    public void twoPointerScored(View v){
-        displayForTeamA(2);
+    public void twoPointerScoredForTeamA(View v){
+        this.scoreTeamA += 2;
+        displayForTeamA(this.scoreTeamA);
     }
 
-    public void freeThrowScored(View v){
-        displayForTeamA(1);
+    public void freeThrowScoredForTeamA(View v){
+        this.scoreTeamA += 1;
+        displayForTeamA(this.scoreTeamA);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void threePointerScoredForTeamB(View v){
+        this.scoreTeamB += 3;
+        displayForTeamB(this.scoreTeamB);
+    }
+
+    public void twoPointerScoredForTeamB(View v){
+        this.scoreTeamB += 2;
+        displayForTeamB(this.scoreTeamB);
+    }
+
+    public void freeThrowScoredForTeamB(View v){
+        this.scoreTeamB += 1;
+        displayForTeamB(this.scoreTeamB);
     }
 }
